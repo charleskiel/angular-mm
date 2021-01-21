a13ee00dfefdd8006814d624caacd85bd17420ae
Charles Kiel <charleskiel@yahoo.com>
2021-01-20 19:18:40 -0700

    baseline functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { EventEmitter,Component, OnInit, Output} from '@angular/core';
+import { io } from 'socket.io-client';
+import {SocketioService} from './socketio.service';
+import {Stock} from './stock/stock'
+//import {SocketComponent} from './socket/socket.component'
+import {Socket} from './socket/socket'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-mm';
+export class AppComponent  implements  OnInit{
+  stockList = [];
+  title = 'Market Manager';
+  name = '';
+  page = 0;
+  socket = new Socket;
+
+  constructor(private socketService: SocketioService) {}
+  
+  ngOnInit() {
+    //this.socketService.setupSocketConnection();
+    console.log("Connecting")
+    this.socket.connect()
+    this.socket.event.on("test",() => {console.log("test Sucsess")})
+    this.socket.event.on("data",data => this.onData(data))
+  }
+
+  onStockAdded(stock:Stock ) {
+    this.stockList[stock.key] = stock;
+  }
+
+  onData(data : object) {
+    console.log(data)
+  }
+
+  showPage(page: number) {
+    this.page = page;
+  }
+
+  //setSelectedWatchlist : (list) => { console.log(`Setting Watchlist to ${list}`); this.setState({ selectedWatchlist: list }); console.log(this.state)},
+  //setSelectedStock: (stock) => { console.log(`Setting Chart to ${stock}`); this.setState({ selectedStock: stock }) },
+  //subscribe: (command, keys = [], dataTypes = []) => { this.sendMsg({"service": "SUB", "command" : command, "keys" :keys, "dataTypes": dataTypes })}, 
+  //getWatchLists : () => { this.setState({ watchlists: Data.getWatchLists() })},
+  //switchView : (page) => {this.setState({ showpage: page })},
+  //setCommandkey : (key) => {
+  //console.log(key);
+  //this.setState((prevState) => { return {settings : {...prevState.settings,...{commandKey: key} }}})
+  //this.ws.send(JSON.stringify({requests: [{service: "ADMIN",command: "SETCOMMANDKEY",commandKey: key,requestid: this.requestid()}]}))
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
+import { NavBarComponent } from './nav-bar/nav-bar.component'
+//import { StatusBarComponent } from './stat-bar/status-bar.component'
 import { AppComponent } from './app.component';
+import { StockComponent } from './stock/stock.component';
+//import { SocketComponent } from './socket/socket.component';
+import { StockListComponent } from './stock-list/stock-list.component';
+import {SocketioService} from './socketio.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import {Stock} from './stock/stock';
+import { StatusBarComponent } from './status-bar/status-bar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockComponent,
+    //SocketComponent,
+    StockListComponent,
+    NavBarComponent,
+    StatusBarComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HighchartsChartModule
+    //SocketComponent,
+
   ],
-  providers: [],
+  providers: [SocketioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
